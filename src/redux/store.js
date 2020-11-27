641986fc8632053bc908cf8a98388aacb4725bad
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-27 18:32:08 +0800

    created store for save or transfer data

---
+++ src/redux/store.js
@@ -0,0 +1,22 @@
+import {createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import promiseMiddleware from 'redux-promise-middleware';
+import rootReducers from './reducers';
+import AsyncStorages from '@react-native-async-storage/async-storage';
+import {persistStore, persistReducer} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorages,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+export default () => {
+  let store = createStore(
+    persistedReducer,
+    applyMiddleware(promiseMiddleware, logger),
+  );
+  const persistore = persistStore(store);
+  return {store, persistore};
+};
