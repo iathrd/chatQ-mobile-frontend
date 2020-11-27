c8dd5446a514297a9c5452a879a621a9d53af718
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-27 18:28:29 +0800

    created auth reducer

---
+++ src/redux/reducers/auth.js
@@ -0,0 +1,48 @@
+const initialState = {
+  isLogin: true,
+  token: '',
+  isLoading: false,
+  isError: false,
+  alertMsg: '',
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOGIN_PENDING': {
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+        alertMsg: 'Login ...',
+      };
+    }
+    case 'LOGIN_FULFILLED': {
+      return {
+        ...state,
+        isLoading: false,
+        isLogin: false,
+        isError: false,
+        token: action.payload.data.token,
+      };
+    }
+    case 'LOGIN_REJECTED': {
+      return {
+        ...state,
+        isLogin: false,
+        isError: true,
+        alertMsg: 'Wrong email or password',
+      };
+    }
+
+    case 'LOGOUT': {
+      return {
+        ...state,
+        isLogin: false,
+        token: '',
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
