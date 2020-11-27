765dbe8958fcdc578468450b2009a7758f97dfe7
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-27 18:11:20 +0800

    created Tab navigator

---
+++ src/screens/TabNavigator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+import Chats from './Chats';
+import Calls from './Calls';
+import Status from './Status';
+
+export const MainTab = () => {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        style: {backgroundColor: '#191f1a'},
+        activeTintColor: '#25D366',
+        inactiveTintColor: '#9b9b9b',
+        indicatorStyle: {backgroundColor: '#25D366'},
+      }}>
+      <Tab.Screen name="Chats" component={Chats} />
+      <Tab.Screen name="Status" component={Status} />
+      <Tab.Screen name="Calls" component={Calls} />
+    </Tab.Navigator>
+  );
+};
